2e133555a871d28efaf465ffa800a7d0af12a2fe
Shubham Haldankar <snh16041998@gmail.com>
2026-08-30 22:52:07 +0530

    Add initial diagnostic log migration

---
+++ src/VehicleDiagnosticsDashboard.Infrastructure/Migrations/20260830154042_AddVehicleDiagnosticsDashboardDb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleDiagnosticsDashboard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVehicleDiagnosticsDashboardDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehicleDiagnosticsLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VehicleId = table.Column<string>(type: "text", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    LogType = table.Column<int>(type: "integer", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleDiagnosticsLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleDiagnosticsLogs");
+        }
+    }
+}

6b8bc2b7960d74aae3143248a4071ba9fb4546fa
Shubham Haldankar <snh16041998@gmail.com>
2026-08-30 22:34:04 +0530

    Add EF Core persistence for diagnostic logs

---
+++ src/VehicleDiagnosticsDashboard.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleDiagnosticsDashboard.Domain.Entities;
+
+namespace VehicleDiagnosticsDashboard.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DiagnosticLog> DiagnosticLogs => Set<DiagnosticLog>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(AppDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+}
---
+++ src/VehicleDiagnosticsDashboard.Infrastructure/Persistence/Configurations/DiagnosticLogConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VehicleDiagnosticsDashboard.Domain.Entities;
+
+namespace VehicleDiagnosticsDashboard.Infrastructure.Persistence.Configurations
+{
+    public class DiagnosticLogConfiguration
+    : IEntityTypeConfiguration<DiagnosticLog>
+    {
+        public void Configure(
+            EntityTypeBuilder<DiagnosticLog> builder)
+        {
+            builder.ToTable("VehicleDiagnosticsLogs");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.VehicleId)
+                .HasColumnName("VehicleId")
+                .IsRequired();
+
+            builder.Property(x => x.DateTimeCreated)
+                .HasColumnName("DateTimeCreated")
+                .IsRequired();
+
+            builder.Property(x => x.Code)
+                .HasColumnName("Code")
+                .IsRequired();
+
+            builder.Property(x => x.LogType)
+                .HasColumnName("LogType")
+                .HasConversion<int>()
+                .IsRequired();
+
+            builder.Property(x => x.Message)
+                .HasColumnName("Message");
+        }
+    }
+}

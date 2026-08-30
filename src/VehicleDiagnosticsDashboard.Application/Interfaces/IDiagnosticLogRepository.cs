4b3ad6994c97425388779090ed76999e5e57053c
Shubham Haldankar <snh16041998@gmail.com>
2026-08-31 00:12:59 +0530

    Add diagnostic log repository interface

---
+++ src/VehicleDiagnosticsDashboard.Application/Interfaces/IDiagnosticLogRepository.cs
@@ -0,0 +1,12 @@
+﻿using VehicleDiagnosticsDashboard.Application.DTOs;
+using VehicleDiagnosticsDashboard.Domain.Entities;
+
+namespace VehicleDiagnosticsDashboard.Application.Interfaces
+{
+    public interface IDiagnosticLogRepository
+    {
+        Task AddAsync(DiagnosticLog diagnosticLog);
+
+        Task<IReadOnlyList<DiagnosticLog>> FindAsync(DiagnosticLogSearchCriteria filter);
+    }
+}
